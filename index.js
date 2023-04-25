2a3386059e486e610b61f2878c036efef40f9fd1
TChervonenko <chervonenko.t@gmail.com>
2023-04-25 17:04:46 +0300

    init project

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function sum (num1, num2) {
+    return num1 + num2
+}
+sum (2,7)
+
+function sub (num1, num2) {
+    return num1 - num2
+}
+sub (34,2)
+function mult (num1, num2) {
+    return num1 * num2
+}
+mult(25,5)
